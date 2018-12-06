a6aba1f78f7d756bfcea504ef5280400cb3bcfb2
E Pogrebnyak <e.pogrebnyak@gmail.com>
2018-12-06 11:45:28 +0300

    sample test

---
+++ test/sample.jl
@@ -0,0 +1,10 @@
+using Test
+import LessOLS : Sample
+
+x = [[1,2,3] [4,5,6]]
+y = [1,2,3]
+sam = Sample(x, y)
+@test sam.X == [[1,2,3] [4,5,6]]
+@test sam.Y == [1,2,3]
+@test_throws DimensionMismatch Sample([1,2], [1,2,3])
+@test_throws DimensionMismatch Sample(y, x)
