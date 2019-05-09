d19103accf42235cef7340e81655efcad9abb54a
epogrebnyak <e.pogrebnyak@gmail.com>
2019-05-10 00:44:08 +0300

    commented example

--- example.jl
+++ example.jl
@@ -1,9 +1,14 @@
-import LessOLS: normal_sampler, ols, show, betas, equation
+import LessOLS: normal_sampler, ols, show, equation
 
+# create sample with known properties
 ns = normal_sampler(a=0, b=20, β_0=5, β=[1, 2.5], sd_e=0.5)
 sample1 = ns(100000)
+
+# attempt a linear model
 lm1 = ols(sample1, intercept=false)
 show(lm1)
+
+# check the coefficients are about right (for a model with zero intercept)
 function almost_equals(a, b, threshold)
     diff = map(abs, (a-b))
     return all(diff .< threshold)
